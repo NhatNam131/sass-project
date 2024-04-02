59b6323f4b776c227fcc3686617671c3e3f74e79
NhatNam131 <namnnph06519@gmail.com>
2024-04-02 22:58:41 +0700

    Built product detail

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -191,6 +191,28 @@ window.addEventListener('template-loaded', () => {
     });
 });
 
+window.addEventListener('template-loaded', () => {
+    const tabsSelector = 'detail-tab__item';
+    const contentsSelector = 'detail-tab__content';
+
+    const tabActive = `${tabsSelector}--active`;
+    const contentActive = `${contentsSelector}--active`;
+
+    const tabContainers = $$('.js-tabs');
+    tabContainers.forEach((tabContainer) => {
+        const tabs = tabContainer.querySelectorAll(`.${tabsSelector}`);
+        const contents = tabContainer.querySelectorAll(`.${contentsSelector}`);
+        tabs.forEach((tab, index) => {
+            tab.onclick = () => {
+                tabContainer.querySelector(`.${tabActive}`)?.classList.remove(tabActive);
+                tabContainer.querySelector(`.${contentActive}`)?.classList.remove(contentActive);
+                tab.classList.add(tabActive);
+                contents[index].classList.add(contentActive);
+            };
+        });
+    });
+});
+
 // Sign Up
 Validator({
     form: '#form-signup',
